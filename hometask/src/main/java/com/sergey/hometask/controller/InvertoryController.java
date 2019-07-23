84f24f3c3129f4a5a5d4bc5a7b8021fd4d818fea
sergey-muzika <53020095+sergey-muzika@users.noreply.github.com>
2019-07-23 12:12:16 +0300

    Add files via upload

--- hometask/src/main/java/com/sergey/hometask/controller/InvertoryController.java
+++ hometask/src/main/java/com/sergey/hometask/controller/InvertoryController.java
@@ -3,6 +3,7 @@ package com.sergey.hometask.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import io.swagger.annotations.ApiOperation;
 
 @Api(tags = "Invertory Application")
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/items")
 public class InvertoryController {
 	
